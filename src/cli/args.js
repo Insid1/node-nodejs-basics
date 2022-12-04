f09ccfd4aa366da7ba0f700ab49a0098e34f7344
Oleg <66309663+Insid1@users.noreply.github.com>
2022-12-04 10:50:21 +0700

    feat: adds cli/args logic

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const {argv} = process;
+  let result = [];
+
+  const optimizedArgs = argv.slice(2)
+  for (let i = 0; i < optimizedArgs.length -1; i+=2) {
+    const key = optimizedArgs[i].slice(2);
+    const value = optimizedArgs[i+1];
+
+    result.push(`${key} is ${value}`)
+  }
+  console.log(result.join(', '))
 };
 
 parseArgs();
