bfae45a33d6638dd581fca7631ed8ff0fb226f7e
Oleg <66309663+Insid1@users.noreply.github.com>
2022-12-04 21:21:41 +0700

    feat: adds zip logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import {pipeline} from "node:stream/promises";
+import {createReadStream, createWriteStream} from "node:fs";
+import {createGzip} from "node:zlib";
+import {getDirName} from "../helpres/helpers.js";
+
+
+const __dirname = getDirName(import.meta);
+const src = __dirname + 'files/fileToCompress.txt';
+const dest = __dirname + 'files/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+  const source = createReadStream(src);
+  const gzip = createGzip();
+  const destination = createWriteStream(dest);
+
+  await pipeline(source, gzip, destination)
 };
 
 await compress();
