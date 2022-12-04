f87e4553e3973691add965331b735ea5f7c3a0bd
Oleg <66309663+Insid1@users.noreply.github.com>
2022-12-04 23:21:18 +0700

    feat: implements worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import {isMainThread, Worker} from "node:worker_threads";
+import {getDirName} from "../helpres/helpers.js";
+import os from "os";
+
+const numOfCpus = os.cpus().length;
+const __dirname = getDirName(import.meta);
+const INCREMENT = 10;
+
+
+function runFibonacciWorker(data) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(`${__dirname}worker.js`, {workerData: data});
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    });
+  });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  if (isMainThread) {
+    const arr = [];
+    for (let i = INCREMENT; i < numOfCpus + INCREMENT; i++) {
+      arr.push(runFibonacciWorker(i));
+    }
+    const calculations = await Promise.allSettled(arr)
+
+    console.log(calculations.map(({status, value}) => ({
+      status: status === 'fulfilled' ? 'resolved' : 'error',
+      value: value || null,
+    })));
+  }
 };
 
-await performCalculations();
+await performCalculations()
