ab467bcfd0d009ee22aa1abf6c2b57c441221d6a
Oleg <66309663+Insid1@users.noreply.github.com>
2022-12-04 11:15:02 +0700

    feat: calc hash implemented

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
-const calculateHash = async () => {
-    // Write your code here 
+import {createHash} from 'node:crypto';
+import {createReadStream} from 'node:fs';
+import {readFile} from 'node:fs/promises';
+import {getDirName} from "../helpres/helpers.js";
+const hash = createHash('sha256');
+
+const __dirname = getDirName(import.meta);
+const src = __dirname + 'files/fileToCalculateHashFor.txt';
+
+export const calculateHash = async () => {
+  const data = await readFile(src);
+  const hashedFile = hash.update(data);
+  console.log(hashedFile.digest('hex'))
+
+  // as stream
+
+  // const input = createReadStream(src);
+  //
+  // input.on('readable', () => {
+  //   const data = input.read();
+  //   if (data)
+  //     hash.update(data);
+  //   else {
+  //     console.log(`${hash.digest('hex')} ${src}`);
+  //   }
+  // })
 };
 
-await calculateHash();
+calculateHash();
